486f3a19e28636e91cc62d8b8c48d0b545dec088
ru482008 <ru482008@gmail.com>
2025-07-17 12:52:19 +0800

    20250717

--- food_recognition/admin.py
+++ food_recognition/admin.py
@@ -2,9 +2,42 @@ from django.contrib import admin
 from .models import FoodNutrition, RecogLog, RecogModel
 from import_export.admin import ExportMixin, ImportMixin, ImportExportModelAdmin
 from django.utils import timezone
+from import_export import resources, fields
 
+
+class FoodNutritionResource(resources.ModelResource):
+    # 指定要匯入的欄位
+    food_name_zh = fields.Field(attribute='food_name_zh', column_name='food_name_zh')
+    food_name_en = fields.Field(attribute='food_name_en', column_name='food_name_en')
+    calories = fields.Field(attribute='calories', column_name='calories')
+    protein = fields.Field(attribute='protein', column_name='protein')
+    carbs = fields.Field(attribute='carbs', column_name='carbs')
+
+    class Meta:
+        model = FoodNutrition
+        import_id_fields = ('food_name_zh', 'food_name_en')  # 根據中英文名稱作為唯一鍵
+        # fields = ('food_name_zh', 'food_name_en', 'calories', 'protein', 'carbs')  # 只匯入這些
+        fields = ('food_name_zh', 'food_name_en', 'calories', 'protein', 'carbs', 'create_user', 'update_user', 'is_recog')
+        skip_unchanged = True
+        report_skipped = True
+
+    def __init__(self, *args, **kwargs):
+        self.context = kwargs.pop('context', {})
+        super().__init__(*args, **kwargs)
+
+    def before_import_row(self, row, row_number=None, **kwargs):
+        user = None
+        if 'request' in self.context:
+            user = self.context['request'].user
+
+        row['is_recog'] = True
+        row['create_user'] = user.username if user else 'import_user'
+        row['update_user'] = user.username if user else 'import_user'
+        
+        
 @admin.register(FoodNutrition)
 class FoodNutritionAdmin(ImportExportModelAdmin):
+    resource_class = FoodNutritionResource
     readonly_fields = ('create_user', 'create_date', 'update_date', 'update_user')  # 管理頁面不可編輯
 
     # 新增頁面不顯示更新欄位（exclude 就是排除欄位）
@@ -22,20 +55,23 @@ class FoodNutritionAdmin(ImportExportModelAdmin):
             obj.update_date = timezone.now()
             obj.update_user = request.user.username
         super().save_model(request, obj, form, change)
+    
+    def get_import_resource_kwargs(self, request, *args, **kwargs):
+        return {'context': {'request': request}}
 
 
 admin.site.register(RecogLog)
 
 @admin.register(RecogModel)
-class RecogModelAdmin(ImportExportModelAdmin):
-    readonly_fields = ('model_path','create_user', 'create_date', 'update_date', 'update_user')  # 管理頁面不可編輯
+class RecogModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('model_file','create_user', 'create_date', 'update_date', 'update_user')  # 管理頁面不可編輯
 
     # 新增頁面不顯示更新欄位（exclude 就是排除欄位）
-    exclude = ('model_path','create_user', 'create_date', 'update_date', 'update_user')
+    exclude = ('model_file','create_user', 'create_date', 'update_date', 'update_user')
     
     def save_model(self, request, obj, form, change):
-        if obj.model_file:
-            obj.model_path = obj.model_file.name 
+        if obj.model_path:
+            obj.model_file = obj.model_path.name 
         if change:
             # 修改時自動更新
             obj.update_date = timezone.now()
--- food_recognition/models.py
+++ food_recognition/models.py
@@ -2,6 +2,15 @@ from django.db import models
 import os
 
 
+DISH = 1
+FOOD = 2
+
+MODEL_TYPE_CHOICES = [
+    (DISH, "菜餚"),
+    (FOOD, "食品"),
+]
+
+
 class FoodNutrition(models.Model):
     food_name_en = models.CharField("食物名稱_英", max_length=50)
     food_name_zh = models.CharField("食物名稱_中", max_length=50)
@@ -12,40 +21,60 @@ class FoodNutrition(models.Model):
     create_user = models.CharField("建立者", max_length=20)
     create_date = models.DateTimeField("建立時間", auto_now_add=True)
     update_user = models.CharField("更新者", max_length=20)
-    update_date = models.DateTimeField("更新時間", auto_now=True)        
+    update_date = models.DateTimeField("更新時間", auto_now=True)
 
     class Meta:
-        db_table = 'food_nutrition'
+        db_table = "food_nutrition"
 
     def __str__(self):
         return f"{self.food_name_zh} / {self.food_name_en}"
 
 
+def recogLog_image_name_upload_path(instance, filename):
+    # 上傳到：models/2025_07/模型檔案.pt
+    return f"recogLogs/{instance.create_date.strftime('%Y_%m')}/image_name/{filename}"
+
+
+def recogLog_recog_image_name_upload_path(instance, filename):
+    # 上傳到：models/2025_07/模型檔案.pt
+    return f"recogLogs/{instance.create_date.strftime('%Y_%m')}/recog_image_name/{filename}"
+
+
 class RecogLog(models.Model):
+    
     image_name = models.CharField("圖檔名稱", max_length=50)
     image_path = models.CharField("圖檔路徑", max_length=50)
-    recog_image_name = models.CharField("辨識圖檔名稱", max_length=50, null=True, blank=True)
+    recog_image_name = models.CharField(
+        "辨識圖檔名稱", max_length=50, null=True, blank=True
+    )
+    
     recog_content = models.CharField("辨識內容", max_length=200, null=True, blank=True)
     recog_model_id = models.IntegerField("模型ID")
-    model_type = models.IntegerField("辨識類別")  # 1=菜餚, 2=食品
+    model_type = models.IntegerField("辨識類別", choices=MODEL_TYPE_CHOICES)
     create_ip = models.CharField("建立者IP", max_length=20)
     create_date = models.DateTimeField("建立時間")
 
     class Meta:
-        db_table = 'food_recog_log'
+        db_table = "food_recog_log"
 
     def __str__(self):
         return self.image_name
+
 
 def model_upload_path(instance, filename):
     # 上傳到：models/2025_07/模型檔案.pt
     return f"models/{instance.create_date.strftime('%Y_%m')}/{filename}"
 
+
 class RecogModel(models.Model):
+
     model_name = models.CharField("模型名稱", max_length=50)
-    model_type = models.IntegerField("辨識類別")  # 1=菜餚, 2=食品
-    model_file = models.FileField("上傳模型檔案", upload_to=model_upload_path, blank=True, null=True)
-    model_path = models.CharField("模型檔案路徑", max_length=100, blank=True)  # 儲存上傳後的實體路徑
+    model_type = models.IntegerField("辨識類別", choices=MODEL_TYPE_CHOICES)
+    # model_file = models.FileField("上傳模型檔案", upload_to=model_upload_path, blank=True, null=True)
+
+    model_file = models.CharField("模型檔案名稱", max_length=100, blank=True, null=True)
+    model_path = models.FileField("上傳模型檔案", upload_to=model_upload_path, blank=True, null=True)
+      
     recog_items = models.TextField("可辨識項目", max_length=1000)
     is_enabled = models.BooleanField("是否啟用", default=False)
     memo = models.TextField("備註", max_length=1000, null=True, blank=True)
@@ -55,7 +84,7 @@ class RecogModel(models.Model):
     update_date = models.DateTimeField("更新時間", auto_now=True)
 
     class Meta:
-        db_table = 'food_recognition_recog_model'
+        db_table = "food_recog_model"
 
     def __str__(self):
         return self.model_name
